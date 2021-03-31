55ad813bf402539288f7c9d0f7b90b54da58ae6f
t-ikeda-dss <61813087+t-ikeda-dss@users.noreply.github.com>
2021-04-01 06:43:55 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,22 +13,22 @@ class App extends Component {
     // page transition
     window.location.href = '../api/search.html';
     return (
-    //  <div className="App">
-    //    <header className="App-header">
-    //      <img src={logo} className="App-logo" alt="logo" />
-    //      <p>
-    //        Edit <code>src/App.js</code> and save to reload.
-    //      </p>
-    //      <a
-    //        className="App-link"
-    //        href="https://reactjs.org"
-    //        target="_blank"
-     //       rel="noopener noreferrer"
-     //     >
-     //       Learn React
-     //     </a>
-     //   </header>
-     // </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     );
   }
 }
