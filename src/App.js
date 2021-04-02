2d0489f355d806a6b60eb87b2341ffd9331fbb54
t-ikeda-dss <61813087+t-ikeda-dss@users.noreply.github.com>
2021-04-02 10:20:51 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,12 +10,45 @@ Amplify.configure(aws_exports);
 //--import { callSearchApi } from './script.js';
 
 class App extends Component {
+  
+  const useScript = url => {
+    useEffect(() => {
+      const script = document.createElement('script');
+
+      script.src = url;
+      script.async = true;
+
+      document.body.appendChild(script);
+
+      return () => {
+        document.body.removeChild(script);
+      }
+    }, [url]);
+  };
+
   componentDidMount() {
     //@@
-    const script = document.createElement("script");
-    script.src = "/api/js/script.js";
-    script.async = true;
-    document.body.appendChild(script);
+    useScript('https://sdk.amazonaws.com/js/aws-sdk-2.7.19.min.js');
+    useScript('https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js');
+    useScript('/api/js/script.js');
+    useScript('/api/js/apigClient.js.js');
+    useScript('/api/js/amazon-cognito-identity.min.js');
+    useScript('/api/js/lib/axios/dist/axios.standalone.js');
+    useScript('/api/js/lib/CryptoJS/rollups/hmac-sha256.js');
+    useScript('/api/js/lib/CryptoJS/rollups/sha256.js');
+    useScript('/api/js/lib/CryptoJS/components/hmac.js');
+    useScript('/api/js/lib/CryptoJS/components/enc-base64.js');
+    useScript('/api/js/lib/url-template/url-template.js');
+    useScript('/api/js/lib/apiGatewayCore/sigV4Client.js');
+    useScript('/api/js/lib/apiGatewayCore/apiGatewayClient.js');
+    useScript('/api/js/lib/apiGatewayCore/simpleHttpClient.js');
+    useScript('/api/js/lib/apiGatewayCore/utils.js');
+    
+    //@@
+    //--const script = document.createElement("script");
+    //--script.src = "/api/js/script.js";
+    //--script.async = true;
+    //--document.body.appendChild(script);
     //++
     fetch(file)
       .then( res => res.text() )
