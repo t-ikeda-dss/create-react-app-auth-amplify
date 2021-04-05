ed26b112922a541cd64eadedc0c09f089737fb57
t-ikeda-dss <61813087+t-ikeda-dss@users.noreply.github.com>
2021-04-05 10:34:51 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react';
 import file from './api/search.htm';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator } from 'aws-amplify-react'
-//--import { withAuthenticator } from '@aws-amplify/ui-react';
-//--import { withMyAuthenticator } from './MyAuth'
+//--import { withAuthenticator } from 'aws-amplify-react'
+import { withMyAuthenticator } from './MyAuth'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
@@ -47,5 +46,5 @@ class App extends Component {
   }
 }
 
-export default withAuthenticator(App, true);
-//--export default withMyAuthenticator(App, true);
+//--export default withAuthenticator(App, true);
+export default withMyAuthenticator(App, true);
