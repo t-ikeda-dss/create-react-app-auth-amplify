9300fc234e2f0e31f5a46d4a9ff0d8347d6b2e7d
t-ikeda-dss <61813087+t-ikeda-dss@users.noreply.github.com>
2021-04-06 13:46:01 +0900

    Update MySignUp.js

--- src/MySignUp.js
+++ src/MySignUp.js
@@ -13,7 +13,7 @@ import {
     Link,
 } from 'aws-amplify-react';
 //--import theme from "./theme";
-//--import { AmplifyTheme } from 'aws-amplify-react';
+import { AmplifyTheme } from 'aws-amplify-react';
 //--import aws_exports from './aws-exports';
 import awsconfig from './aws-exports';
 //--Amplify.configure(aws_exports);
