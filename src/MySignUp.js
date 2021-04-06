295d2b79ee97a8cc846f1b6aae30364d24167bcc
t-ikeda-dss <61813087+t-ikeda-dss@users.noreply.github.com>
2021-04-06 15:48:41 +0900

    Update MySignUp.js

--- src/MySignUp.js
+++ src/MySignUp.js
@@ -38,58 +38,81 @@ export default class MySignUp extends SignUp {
             .then(() => this.changeState('confirmSignUp', username))
             .catch(err => this.error(err));
     }
-    
-    showComponent(theme) {
-        const { hide } = this.props;
-        if (hide && hide.includes(MySignUp)) { return null; }
 
-        return (
-            <FormSection data-component="FormSection" theme={theme}>
-              <div className="App">
-                <SectionHeader theme={theme}>サインアップ</SectionHeader>
-                <SectionBody theme={theme}>
-                    <InputRow 
-                        autoFocus
-                        placeholder="ユーザー名入力"
-                        theme={theme}
-                        key="username"
-                        name="username"
-                        onChange={this.handleInputChange}
-                        data-component="InputRow"
-                    />
-                    <InputRow
-                        placeholder="パスワード"
-                        theme={theme}
-                        type="password"
-                        key="password"
-                        name="password"
-                        onChange={this.handleInputChange}
-                    />
-                    <InputRow
-                        placeholder="メールアドレス"
-                        theme={theme}
-                        key="email"
-                        name="email"
-                        onChange={this.handleInputChange}
-                    />
-                    <button data-component="Button" class="Button" onClick={this.signUp} theme={AmplifyTheme}>
-                        サインアップ
-                    </button>
-                </SectionBody>
-                <SectionFooter theme={theme}>
-                    <div style={theme.col6}>
-                        <Link theme={theme} onClick={() => this.changeState('confirmSignUp')}>
-                            {I18n.get('Confirm a Code')}
-                        </Link>
-                    </div>
-                    <div style={theme.col6}>
-                        <Link theme={theme} onClick={() => this.changeState('signIn')}>
-                            サインイン
-                        </Link>
-                    </div>
-                </SectionFooter>
-              </div>
-            </FormSection>
-        )
-    }
+	showComponent(theme): React.ReactNode {
+		const { hide } = this.props;
+		if (hide && hide.includes(SignUp)) {
+			return null;
+		}
+		if (this.checkCustomSignUpFields()) {
+			this.signUpFields = this.props.signUpConfig.signUpFields;
+		}
+		this.sortFields();
+		return (
+			<FormSection theme={theme} data-test={auth.signUp.section}>
+				<SectionHeader theme={theme} data-test={auth.signUp.headerSection}>
+					{I18n.get(this.header)}
+				</SectionHeader>
+				<SectionBody theme={theme} data-test={auth.signUp.bodySection}>
+					{this.signUpFields.map(field => {
+						return field.key !== 'phone_number' ? (
+							<FormField theme={theme} key={field.key}>
+								{field.required ? (
+									<InputLabel theme={theme}>
+										{I18n.get(field.label)} *
+									</InputLabel>
+								) : (
+									<InputLabel theme={theme}>{I18n.get(field.label)}</InputLabel>
+								)}
+								<Input
+									autoFocus={
+										this.signUpFields.findIndex(f => f.key === field.key) === 0
+									}
+									placeholder={I18n.get(field.placeholder)}
+									theme={theme}
+									type={field.type}
+									name={field.key}
+									key={field.key}
+									onChange={this.handleInputChange}
+									data-test={auth.signUp.nonPhoneNumberInput}
+								/>
+							</FormField>
+						) : (
+							<PhoneField
+								theme={theme}
+								required={field.required}
+								defaultDialCode={this.getDefaultDialCode()}
+								label={field.label}
+								placeholder={field.placeholder}
+								onChangeText={this.onPhoneNumberChanged}
+								key="phone_number"
+							/>
+						);
+					})}
+				</SectionBody>
+				<SectionFooter theme={theme} data-test={auth.signUp.footerSection}>
+					<SectionFooterPrimaryContent theme={theme}>
+						<Button
+							disabled={this.state.requestPending}
+							onClick={this.signUp}
+							theme={theme}
+							data-test={auth.signUp.createAccountButton}
+						>
+							{I18n.get('Create Account')}
+						</Button>
+					</SectionFooterPrimaryContent>
+					<SectionFooterSecondaryContent theme={theme}>
+						{I18n.get('Have an account? ')}
+						<Link
+							theme={theme}
+							onClick={() => this.changeState('signIn')}
+							data-test={auth.signUp.signInLink}
+						>
+							{I18n.get('Sign in')}
+						</Link>
+					</SectionFooterSecondaryContent>
+				</SectionFooter>
+			</FormSection>
+		);
+	}
 }
