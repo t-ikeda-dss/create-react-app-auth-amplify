f311afdc646114bcb877068f54db404be98d878d
t-ikeda-dss <61813087+t-ikeda-dss@users.noreply.github.com>
2021-04-07 10:43:17 +0900

    Update MyAuth.js

--- src/MyAuth.js
+++ src/MyAuth.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { ConfirmSignIn, ConfirmSignUp, ForgotPassword, SignIn, SignOut, VerifyContact, withAuthenticator } from 'aws-amplify-react';
 import MySignUp from './MySignUp'
+import { AmplifyTheme } from 'aws-amplify-react';
 
-export function withMyAuthenticator(Comp, includeGreetings, theme) {
-  return withAuthenticator(Comp, {includeGreetings: includeGreetings, theme: theme }, [
+const myTheme = {
+    ...AmplifyTheme,
+    BackgroundColor: { color: 'blue',backgroundColor: 'blue' },
+    button: { color: 'green',backgroundColor: 'green' },
+    amazonSignInButton: { color: 'blue',backgroundColor: 'blue' },
+    signInButton: { backgroundColor: 'blue' , color: 'blue'}
+};
+
+export function withMyAuthenticator(Comp, greetings) {
+  return withAuthenticator(Comp, {includeGreetings: greetings, theme: myTheme }, [
       <SignIn/>,
       <SignOut/>,
       <ConfirmSignIn/>,
