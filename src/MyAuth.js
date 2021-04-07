fd649445d4358e71c11a17b1e064fce23bec01b2
t-ikeda-dss <61813087+t-ikeda-dss@users.noreply.github.com>
2021-04-07 09:41:09 +0900

    Update MyAuth.js

--- src/MyAuth.js
+++ src/MyAuth.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { ConfirmSignIn, ConfirmSignUp, ForgotPassword, SignIn, SignOut, VerifyContact, withAuthenticator } from 'aws-amplify-react';
 import MySignUp from './MySignUp'
 
-export function withMyAuthenticator(Comp, includeGreetings=false) {
-  return withAuthenticator(Comp, includeGreetings, [
+export function withMyAuthenticator(Comp, setting) {
+  return withAuthenticator(Comp, setting, [
       <SignIn/>,
       <SignOut/>,
       <ConfirmSignIn/>,
