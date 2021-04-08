23086146b4ddedf6b726f55c947e56caad175a69
t-ikeda-dss <61813087+t-ikeda-dss@users.noreply.github.com>
2021-04-08 10:24:24 +0900

    Create aws-exports2.js

---
+++ src/aws-exports2.js
@@ -0,0 +1,8 @@
+const awsmobile = {
+  "aws_project_region": "ap-northeast-1",
+  "aws_cognito_identity_pool_id": "ap-northeast-1:f749b712-c439-48f4-9fa5-a7ce66fdbc25",
+  "aws_cognito_region": "ap-northeast-1",
+  "aws_user_pools_id": "ap-northeast-1_S01Kqn7pX",
+  "aws_user_pools_web_client_id": "30j2k1vbmbsc85hqbi7omo8pp6",
+  "oauth": {}
+};
