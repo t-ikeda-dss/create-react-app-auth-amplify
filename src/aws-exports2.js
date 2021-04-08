2cf277ad5b71029212eb3e5a810fb81b5ba8aa83
t-ikeda-dss <61813087+t-ikeda-dss@users.noreply.github.com>
2021-04-08 10:44:56 +0900

    Update aws-exports2.js

--- src/aws-exports2.js
+++ src/aws-exports2.js
@@ -1,8 +1,10 @@
 const awsmobile = {
   "aws_project_region": "ap-northeast-1",
-  "aws_cognito_identity_pool_id": "ap-northeast-1:f749b712-c439-48f4-9fa5-a7ce66fdbc25",
+  "aws_cognito_identity_pool_id": "ap-northeast-1:e7495629-4820-4a34-a4ab-2858a8592b3c",
   "aws_cognito_region": "ap-northeast-1",
-  "aws_user_pools_id": "ap-northeast-1_S01Kqn7pX",
-  "aws_user_pools_web_client_id": "30j2k1vbmbsc85hqbi7omo8pp6",
+  "aws_user_pools_id": "ap-northeast-1_Pif7YAiJv",
+  "aws_user_pools_web_client_id": "ap-northeast-1_Pif7YAiJv",
   "oauth": {}
 };
+
+export default awsmobile;
