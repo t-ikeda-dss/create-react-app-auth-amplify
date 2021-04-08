f0cc249d6915fb2d2d7ceab448797086740e348c
t-ikeda-dss <61813087+t-ikeda-dss@users.noreply.github.com>
2021-04-08 10:25:09 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports';
+import awsconfig from './aws-exports2';
 
 Amplify.configure(awsconfig);
 
