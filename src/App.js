02fbff31d73008bc0f7a9bb2f2a744b277840b15
t-ikeda-dss <61813087+t-ikeda-dss@users.noreply.github.com>
2021-04-08 15:30:41 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-//--import { FormSection, SectionHeader, SectionBody, SectionFooter, InputRow, ButtonRow, Link, } from 'aws-amplify-react';
 import file from './api/search.htm';
 import logo from './logo.svg';
 import './App.css';
-//--import { withMyAuthenticator } from './MyAuth';
 import Amplify, { Auth, API } from 'aws-amplify';
 import { AmplifyAuthenticator, AmplifyContainer, AmplifySignIn, AmplifySignOut } from '@aws-amplify/ui-react'; 
 import { AmplifyButton, AmplifyInput } from '@aws-amplify/ui-react'; 
@@ -13,25 +11,20 @@ import aws_exports from './aws-exports2';
 Amplify.configure(aws_exports);
 
 Amplify.configure({
-    // OPTIONAL - if your API requires authentication 
-    Auth: {
-        // REQUIRED - Amazon Cognito Identity Pool ID
-        identityPoolId: 'ap-northeast-1:e7495629-4820-4a34-a4ab-2858a8592b3c',
-        // REQUIRED - Amazon Cognito Region
-        region: 'ap-northeast-1', 
-        // OPTIONAL - Amazon Cognito User Pool ID
-        userPoolId: 'ap-northeast-1_Pif7YAiJv', 
-        // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-        userPoolWebClientId: '57b2eetaf7vo2jo7gkvj2g0g4d',
-    },
-    API: {
-        endpoints: [
-            {
-                name: "SearchFunction",
-                endpoint: "https://gf3u303pmb.execute-api.ap-northeast-1.amazonaws.com/default/SearchFunction"
-            }
-        ]
-    }
+  Auth: {
+    identityPoolId: 'ap-northeast-1:e7495629-4820-4a34-a4ab-2858a8592b3c',
+    region: 'ap-northeast-1', 
+    userPoolId: 'ap-northeast-1_Pif7YAiJv', 
+    userPoolWebClientId: '57b2eetaf7vo2jo7gkvj2g0g4d',
+  },
+  API: {
+    endpoints: [
+      {
+        name: "SearchFunction",
+        endpoint: "https://gf3u303pmb.execute-api.ap-northeast-1.amazonaws.com/default/SearchFunction"
+      }
+    ]
+  }
 });
 
 // 
@@ -52,13 +45,9 @@ class App extends React.Component {
     sessionStorage.setItem( "aws_region" , aws_exports.aws_project_region );
     sessionStorage.setItem( "aws_poolid" , aws_exports.aws_user_pools_id );
     sessionStorage.setItem( "aws_clitid" , aws_exports.aws_user_pools_web_client_id );
-    
-    //fetch(file)
-    //  .then( res => res.text() )
-    //  .then( text => document.querySelector('#inner').innerHTML = text );
   }
 
-  GetSearchApi = () => {
+  handleClick = async function () {
     const apiName = 'SearchFunction';
     const path = '';
     //--const user = await Auth.currentAuthenticatedUser();
@@ -74,8 +63,6 @@ class App extends React.Component {
       }
     };
 
-    return API.get(apiName, path, option);
-      
     //API
     //  .get(apiName, path, option)
     //  .then(response => {
@@ -87,21 +74,11 @@ class App extends React.Component {
     //    console.log(error);
     //  });
 
-    //const res = await API.get(apiName, path, option);
+    var res = await API.get(apiName, path, option);
+    console.log(res);
+    var objEmb = document.getElementById('emb');
+  　objEmb.innerHTML = res;
   };
-
-  handleClick = async function () {
-  //handleClick = () => {
-      //var keyword = document.getElementById('scTxt').value;
-      var response = await this.GetSearchApi();
-      var objEmb = document.getElementById('emb');
-    　objEmb.innerHTML = response;
-  }
-    
-  callScript = () => {
-    alert("call script");
-    //--callSearchApi();
-  }
 
   render() {
     return (
@@ -112,9 +89,8 @@ class App extends React.Component {
           <center>
             <legend>検索実行</legend>
             <label >検索文字列 : </label>
-            <AmplifyInput id="scTxt" value="" type="text" placeholder="検索したいキーワードを入力"></AmplifyInput>
-            <AmplifyButton type="button" onclick={this.callScript}>Alert 表示</AmplifyButton>
-            <AmplifyButton type="button" onclick={this.handleClick}>API Call</AmplifyButton>
+            <AmplifyInput id="scTxt" value="" type="text" placeholder="検索キーワード入力"></AmplifyInput>
+            <AmplifyButton type="button" onclick={this.handleClick}>検索</AmplifyButton>
             <div id="emb">
   　        </div>
           </center>
@@ -125,4 +101,3 @@ class App extends React.Component {
 }
 
 export default App;
-//--export default withMyAuthenticator(App, true);
