d0ca23c9729310aa133ad6418b22bfbe99c8e5b9
t-ikeda-dss <61813087+t-ikeda-dss@users.noreply.github.com>
2021-04-09 13:57:21 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ class App extends React.Component {
       
     const option = {
       headers: {
-        //--Authorization: token,
+        Authorization: token,
         //--accept: 'text/html'
       },
       'queryStringParameters': {
