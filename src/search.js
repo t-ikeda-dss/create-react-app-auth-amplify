87b13c81fad481c8b54d20d22aa4b0f2a7066a94
t-ikeda-dss <61813087+t-ikeda-dss@users.noreply.github.com>
2021-04-12 14:57:45 +0900

    Update search.js

--- src/search.js
+++ src/search.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import file from './api/search.htm';
 import logo from './logo.svg';
 import './App.css';
 import Amplify, { Auth, API } from 'aws-amplify';
 import { AmplifyAuthenticator, AmplifyContainer, AmplifySignIn, AmplifySignOut } from '@aws-amplify/ui-react'; 
 import { AmplifyButton, AmplifyInput } from '@aws-amplify/ui-react'; 
 import { I18n } from 'aws-amplify';
-import { vocabularies } from './vocabularies2';
-import aws_exports from './aws-exports2';
+//--import { vocabularies } from './vocabularies2';
+import aws_exports from './aws-exports';
+//--import aws_exports from './aws-exports2';
 Amplify.configure(aws_exports);
 
 Amplify.configure({
@@ -38,7 +38,7 @@ Amplify.configure({
 });
 
 // 
-I18n.putVocabularies(vocabularies);
+//--I18n.putVocabularies(vocabularies);
 I18n.setLanguage('ja');
 
 class search extends React.Component {
