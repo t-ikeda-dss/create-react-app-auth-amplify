b6d936af390acd16d58e8f8f9de1180da42b8ed5
t-ikeda-dss <61813087+t-ikeda-dss@users.noreply.github.com>
2021-04-12 14:40:47 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import './App.css';
 import { AmplifyAuthenticator, AmplifyContainer, AmplifySignIn, AmplifySignOut } from '@aws-amplify/ui-react'; 
 import { AmplifyButton, AmplifyInput } from '@aws-amplify/ui-react'; 
 import { I18n } from 'aws-amplify';
-import { vocabularies } from './vocabularies2';
+//--import { vocabularies } from './vocabularies2';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import mainmenu from './mainmenu';
 import search from './search';
-import aws_exports from './aws-exports2';
+import aws_exports from './aws-exports';
+//--import aws_exports from './aws-exports2';
 Amplify.configure(aws_exports);
 
 // 
-I18n.putVocabularies(vocabularies);
+//--I18n.putVocabularies(vocabularies);
 I18n.setLanguage('ja');
 
 class App extends React.Component {
