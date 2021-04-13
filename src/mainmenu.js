6013fa0ded6f452c1c73d8db0ad0335430809008
t-ikeda-dss <61813087+t-ikeda-dss@users.noreply.github.com>
2021-04-13 13:40:15 +0900

    Update mainmenu.js

--- src/mainmenu.js
+++ src/mainmenu.js
@@ -2,15 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
  
 class mainmenu extends React.Component {
- 
+
+    onLoad = () => {
+      alert("call onLoad");
+    }
+
     render() {
         return (
+          <body onload="this.onLoad"> 　
             <div>
               Main Menu!!!
               <br/>
               <input id="lastword" value={sessionStorage.getItem("api-search-keyword")} type="text"></input>
               <Link to={`/search`}>検索画面を表示</Link>
             </div>
+          </body>
         );
     }
 }
